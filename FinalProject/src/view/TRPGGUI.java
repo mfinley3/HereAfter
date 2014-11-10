2d4769adcf60597843861f07c8d75c4a15775976
Katelyn Hudspeth <katelynhudspeth@Katelyns-MacBook-Pro.local>
2014-11-10 16:07:01 -0700

    Added background to TRPGGUI, implemented mouse listener

--- FinalProject/src/view/TRPGGUI.java
+++ FinalProject/src/view/TRPGGUI.java
@@ -3,33 +3,41 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
  * 
- * Sets up the GUI.  Starts with the options of "New Game", "Continue" or "Quit" and dynamically changes while buttons are selected.
+ * Sets up the GUI. Starts with the options of "New Game", "Continue" or "Quit"
+ * and dynamically changes while buttons are selected.
  * 
  * 
  * @author Katelyn
- *
+ * 
  */
 
 public class TRPGGUI extends JFrame {
-	//Random serial number
+	// Random serial number
 	private static final long serialVersionUID = -703102129384036053L;
-	
-	private JButton newGame;
-	private JButton loadGame;
-	private JButton quit;
+
 	private JPanel selections;
+	private JLabel background;
+	private boolean firstClick;
 
 	// private Map map;
 
@@ -40,13 +48,12 @@ public class TRPGGUI extends JFrame {
 	public TRPGGUI() {
 
 		// map = new Map();
+		firstClick = true;
 
-		setTitle("______________");
+		setTitle("HereAfter");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		Toolkit tk = Toolkit.getDefaultToolkit();
-		Dimension d = tk.getScreenSize();
-		this.setSize(d.width - 250, d.height - 150);
+		this.setSize(990, 660);
 		this.setVisible(true);
 		this.setLocation(100, 0);
 
@@ -58,90 +65,108 @@ public class TRPGGUI extends JFrame {
 
 	public void startUp() {
 		// add the picture to the startUp
-
 		selections = new JPanel();
-		selections.setLayout(new FlowLayout());
 
-		
-		newGame = new JButton("New Game");
-		
-		loadGame = new JButton("Continue Game");
-		quit = new JButton("Quit");
-		
-		//Icon image = new ImageIcon("pc5ezzEyi.png");
-		//newGame.setIcon(image);
-		
-		//newGame.setContentAreaFilled(false);
-		//newGame.setFocusPainted(false);
-		//newGame.setBorderPainted(false);
+		ImageIcon image = new ImageIcon("background.jpeg");
 
-		selections.add(newGame);
-		selections.add(loadGame);
-		selections.add(quit);
+		background = new JLabel(image);
+		this.add(background);
 
-		this.add(selections, BorderLayout.WEST);
-
-		registerListeners();
+		// registerListeners();
 
 	}
 
 	private void registerListeners() {
 
-		newGame.addActionListener(new NewGameListener());
-		loadGame.addActionListener(new LoadGameListener());
-		quit.addActionListener(new QuitListener());
+		MouseListener listener = new ListenToMouse();
+		MouseMotionListener motionListener = new ListenToMouse();
+
+		selections.addMouseMotionListener(motionListener);
+		selections.addMouseListener(listener);
 
 	}
 
-	private class NewGameListener implements ActionListener {
-		public void actionPerformed(ActionEvent anActionEvent) {
-			if (newGame.getText().equals("New Game")) {
+	/**
+	 * Adds a listener to the mouse.
+	 */
+	private class ListenToMouse implements MouseMotionListener, MouseListener {
 
-				newGame.setText("Easy");
-				loadGame.setText("Medium");
-				quit.setText("Hard");
+		/**
+		 * Event were a mouse button is clicked, Is not used.
+		 * 
+		 * @param evt
+		 *            Takes in a clicked event
+		 */
+		public void mouseClicked(MouseEvent evt) {
+			int clickX = evt.getX();
+			int clickY = evt.getY();
 
-			} else if (newGame.getText().equals("easy")) {
+			if (firstClick) {
 
-				// setLevel = "easy";
-
+				if (clickX > ____ && clickX < ____ && clickY > ____ && clickY < ____) {
+					// This means there is a new game
+					// go to the second page with the level options
+					ImageIcon image2 = new ImageIcon("background2.jpeg");
+					background.setIcon(image2);
+					firstClick = false;
+				} else if(clickX > ____ && clickX < ____ && clickY > ____ && clickY < ____) {
+					//This means they want to continue their game
+					
+					
+					firstClick = false;
+				} else if(clickX > ____ && clickX < ____ && clickY > ____ && clickY < ____) {
+					//This means they want to quit the game
+					System.exit(0);
+				}
 			} else {
-				// Continue last game, load the most recent game
-
-			}
-
-		}
-	}
-
-	private class LoadGameListener implements ActionListener {
-		public void actionPerformed(ActionEvent anActionEvent) {
-			// See if we can load a game from the previous time, so instead of
-			// it asking at the beginning,
-			// have a pop up message happen now so that they can reload the past
-			// information
-			if (loadGame.getText().equals("Continue Game")) {
-				newGame.setText("Continue last game");
-				loadGame.setText("Load game");
-			} else if (loadGame.getText().equals("medium")) {
-				// setLevel = "medium";
-			} else {
-				//Go to a list of saved games so they can select one
+				if (clickX > ____ && clickX < ____ && clickY > ____ && clickY < ____) {
+					// This means the difficulty is easy
+					map.setLevel("easy");
+					background.setIcon(null);
+				} else if(clickX > ____ && clickX < ____ && clickY > ____ && clickY < ____) {
+					//This means the difficulty is medium
+					map.setLevel("medium");
+					background.setIcon(null);
+				} else if(clickX > ____ && clickX < ____ && clickY > ____ && clickY < ____) {
+					//This means the difficulty is hard
+					map.setLevel("hard");
+					background.setIcon(null);
+				}
+				
 				
 			}
 		}
-	}
 
-	private class QuitListener implements ActionListener {
-		public void actionPerformed(ActionEvent anActionEvent) {
-			// Exit the JFrame
-			if (quit.getText().equals("Quit")) {
-				System.exit(0);
-			}
-			// getText().equals("hard")
-			else {
-				// setLevel = "hard";
-			}
+		@Override
+		public void mousePressed(MouseEvent e) {
+
 		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+
+		}
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+
+		}
+
+		@Override
+		public void mouseMoved(MouseEvent e) {
+
+		}
+
 	}
 
 }
