fee7952b92bf321be893197e8fb6f4ade9102134
Brian <seamanb@email.arizona.edu>
2014-11-10 10:45:05 -0700

    Added a starting control file: 11/10, 10:44AM

---
+++ FinalProject/src/model/TRpg.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * The controller of the project. Sends messages to map, Saves Data, Loads Data,
+ * sets up players, caluculate which map is needed, sends messages to the enemy
+ * team factory.
+ * 
+ * @author Brian
+ *
+ */
+public class TRpg {
+
+}
