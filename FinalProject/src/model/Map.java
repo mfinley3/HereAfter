ec08d48e794e6a33d1b265a834ef2c3583926683
Mike <mfinley3@email.arizona.edu>
2014-11-11 13:23:06 -0700

    deleted ItemList.java

--- FinalProject/src/model/Map.java
+++ FinalProject/src/model/Map.java
@@ -1,7 +1,11 @@
 package model;
 
+import java.util.List;
+import java.util.Stack;
+
 import space.Space;
 import space.WasteLandSpace;
+import units.Unit;
 
 public class Map {
 	
@@ -28,9 +32,25 @@ public class Map {
 			}		
 	}
 	
-	public void addUnitToMap(){
+	public void addUnitsToMap(List<Unit> unitList){
 		
+		unitList = new Stack<Unit>();
 		unitsOnMap = new Unit[25][25];
+		
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map.length; j++) {
+				map[i][j] = new WasteLandSpace(); // Map of empty spaces
+			}
+			
+		int k = 0;
+		int r = 2;
+		
+		while(!unitList.isEmpty()){
+			unitsOnMap[k][r] = ((Stack<Unit>) unitList).pop();
+			
+			
+			
+		}
 		
 		
 		
