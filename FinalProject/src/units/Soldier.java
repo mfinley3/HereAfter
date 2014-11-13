4d65405f24c5a8456dd51d80330d317b143b9757
Chioke Aarhus <caarhus@email.arizona.edu>
2014-11-12 17:50:05 -0700

    Changed the type of Unit to an Abstract class, instead of an interface. Should make unit types a bit easier.

--- FinalProject/src/units/Soldier.java
+++ FinalProject/src/units/Soldier.java
@@ -3,111 +3,20 @@ package units;
 import item.Item;
 import item.ItemType;
 
-public class Soldier implements Unit {
+public class Soldier extends Unit {
 
 	private boolean canMove = false;
 	
 	public Soldier() {
+		super();
 		Item givenWeapon = new Item("Rifle", ItemType.ATK);
 		itemList.add(givenWeapon);
 	}
 
 	@Override
-	public int getHealth() {
-		// Health level * health modifier
-		int hpMod = 1;
-		int totalHealth = 100;
-		
-		for (Item item : itemList) {
-			if (item.isHealthItem()) {
-				hpMod += 1;
-				totalHealth = 100 * hpMod;
-			}
-		}
-		return totalHealth;
-	}
-
-	@Override
-	public int attack() {
-		// Attack level * attack modifier
-		int atkMod = 1;
-		for (Item item : itemList) {
-			if (item.isAtkItem()) {
-				atkMod = 2;
-			}
-		}
-		return 10 * atkMod;
-	}
-	
-	@Override
-	public int defense() {
-		// Defense level * attack modifier
-		int atkMod = 1;
-		for (Item item : itemList) {
-			if (item.isAtkItem()) {
-				atkMod = 2;
-			}
-		}
-		return 10 * atkMod;
-	}
-
-	@Override
 	public void visibility() {
 		// TODO Auto-generated method stub
-
-	}
-	
-	@Override
-	public int getMovement() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public boolean canMove() {
-		// TODO Auto-generated method stub
-		return canMove;
-	}
-	
-	@Override
-	public void setCanMove() {
-		canMove = !canMove;
-	}
-	
-	@Override
-	public int getUR() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void setUR(int uR) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public int getUC() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void setHC(int uC) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean isAlive() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public String getStats() {
-		// TODO Auto-generated method stub
-		return null;
+		
 	}
 
 }
--- FinalProject/src/units/Unit.java
+++ FinalProject/src/units/Unit.java
@@ -3,45 +3,77 @@ package units;
 import item.Item;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 
  * @author Chioke
  *
  */
-public interface Unit {
+public abstract class Unit {
 
-	public ArrayList<Item> itemList = new ArrayList<Item>(); // Every Unit gets a blank list of items
-	
-	public int getHealth();
-	
-	public int attack();
-	
-	public int defense();
-	
-	public void visibility();
-	
-	public int getMovement();
-	
-	public boolean canMove();
-	
-	public void setCanMove();
-	
+	// Every Unit gets a blank list of items
+	public ArrayList<Item> itemList = new ArrayList<Item>();
+
+	public int getHealth() {
+		// Health level * health modifier
+		int hpMod = 1;
+		int totalHealth = 100;
+
+		for (Item item : itemList) {
+			if (item.isHealthItem()) {
+				hpMod += 1;
+				totalHealth = 100 * hpMod;
+				// Grabbing a health item will replenish your life
+			}
+		}
+		return totalHealth;
+	}
+
+	public int attack() {
+		return 0;
+	}
+
+	public int defense() {
+		return 0;
+	}
+
+	public abstract void visibility();
+
+	public int getMovement() {
+		return 0;
+	}
+
+	public boolean canMove() {
+		return false;
+	}
+
+	public void setCanMove() {
+	}
+
 	/*
 	 * Methods dealing w/ Unit Locations
 	 */
-	public int getUR(); // Getting their row location
+	public int getUR() {
+		return 0;
+	} // Getting their row location
 
-	public void setUR(int uR); // Setting their row location
+	public void setUR(int uR) {
+	} // Setting their row location
 
-	public int getUC(); // Getting their column location
+	public int getUC() {
+		return 0;
+	} // Getting their column location
 
-	public void setHC(int uC); // Setting their column location
-	
-	public boolean isAlive();
-	
-	public String getStats();
-	
-} //end of class Unit
+	public void setHC(int uC) {
+	} // Setting their column location
+
+	public boolean isAlive() {
+		return false;
+	}
+
+	public String getStats() {
+		return null;
+	}
+
+} // end of class Unit
 
