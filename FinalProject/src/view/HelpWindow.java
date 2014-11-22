d11ceddd972da42c7b98fc1b8ce6775f934bc023
Chioke Aarhus <caarhus@email.arizona.edu>
2014-11-21 18:40:12 -0700

    Changed the messages of the HelpWindow

--- FinalProject/src/view/HelpWindow.java
+++ FinalProject/src/view/HelpWindow.java
@@ -32,31 +32,31 @@ public class HelpWindow extends JFrame implements Runnable {
 		goals.setEditable(false);
 		goals.setBackground(Color.WHITE);
 		goals.setLineWrap(true);
-		goals.setText("GOALS: Your goal is to control the units in the top right hand corner of the map and either capture the tower near the bottom left of the map or kill all the enemies");
-				
+		goals.setText("GOALS: The intent of the game is to cure the world of the new-age Ebola. Your group contains the cure, however, it is low in quantity. To circumvent this issue, your units in the top left-hand corner of the map must either move on top, or 'capture', the Beacon Tower near the bottom right of the map to release the cure via air, or you can kill all of the enemies on the map.");
+		
 		movement = new JTextArea();
 		movement.setEditable(false);
 		movement.setBackground(Color.WHITE);
 		movement.setLineWrap(true);
-		movement.setText("MOVEMENT: To move first select the unit you wish to move then select the soace you wish to move that unit to. Once you have done that click the 'Move' button on the right hand side of the screen.");
+		movement.setText("MOVEMENT: First select the unit you wish to move, then select the space in which you'd like the that unit to move to. Once you have done that, click the 'Move' button on the left-hand side of the screen.");
 		
 		attacks = new JTextArea();
 		attacks.setEditable(false);
 		attacks.setBackground(Color.WHITE);
 		attacks.setLineWrap(true);
-		attacks.setText("ATTACKING: To attack first select the unit of yours you wish to have commit the attack. Then select the target you wish to attack. Once both have been done, press the 'Attack' button." );
+		attacks.setText("ATTACKING: First select the unit you wish to have commit to the attack. Then select the target you wish to attack. Once both have been done, press the 'Attack' button.");
 		
 		item = new JTextArea();
 		item.setEditable(false);
 		item.setBackground(Color.WHITE);
 		item.setLineWrap(true);
-		item.setText("ITEM: Using Items is not yet supported. It is HIGHLY recommended you do not press this button as no one knows what may happen if you do. It could break the universe... (But it will likely just break the game)");
+		item.setText("ITEM: Using Group-Inventory Items is not yet fully supported. When it is, it will give the selected Item in your inventory to every Unit's individual item lists.");
 		
 		wait = new JTextArea();
 		wait.setEditable(false);
 		wait.setBackground(Color.WHITE);
 		wait.setLineWrap(true);
-		wait.setText("WAITING: To wait first select the unit you wish you have wait it out for the turn and then click on the 'Wait' Button. This is used to not move a unit for that turn if you already like where they are.");
+		wait.setText("WAITING: Used in replace of 'Move' or 'Attack', 'Wait' allows the current Unit to remain where they are, by clicking the Unit you'd like to 'Wait', then clicking the 'Wait' button."); 
 		
 		this.add(goals);
 		this.add(movement);
