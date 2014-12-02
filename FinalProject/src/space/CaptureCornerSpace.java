d33d354834dae6422ea0d118e6b846506fb240a2
Mike <mfinley3@email.arizona.edu>
2014-12-02 11:42:10 -0700

    added a hasBeenCaptured boolean to cornerTower

--- FinalProject/src/space/CaptureCornerSpace.java
+++ FinalProject/src/space/CaptureCornerSpace.java
@@ -5,6 +5,7 @@ package space;
  */
 public class CaptureCornerSpace extends Space{
 
+	private Boolean hasBeenCaptured;
 	/**
 	 * Instantiates a new capture corner space.
 	 * SpaceType/IsTheSpaceWalkable/IsThe spaceOccupied/IsTheSpaceVisible/MoveHindranceOfTheSpace/VisiablityOfTheSpace
@@ -12,6 +13,18 @@ public class CaptureCornerSpace extends Space{
 	public CaptureCornerSpace() {
 		super("CaptureCorner", true, false, true, 1, 2);
 		
+	}
+	/**
+	 * @return the hasBeenCaptured
+	 */
+	public Boolean getHasBeenCaptured() {
+		return hasBeenCaptured;
+	}
+	/**
+	 * @param hasBeenCaptured the hasBeenCaptured to set
+	 */
+	public void setHasBeenCaptured(Boolean hasBeenCaptured) {
+		this.hasBeenCaptured = hasBeenCaptured;
 	}
 
 }
